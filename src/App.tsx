9b7a9269cfead039c9710bb99743052ee26c1191
greg-hodgson <greg.hodgson1@gmail.com>
2021-10-28 00:03:15 +1100

    first commit (go me!)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import useGetAirtableData from './useGetAirtableData';
+
+type ProcessEnv = string | undefined;
+
 function App() {
+
+  const reactAppBaseID: ProcessEnv = process.env.REACT_APP_BASE_ID;
+
+  const data = useGetAirtableData(reactAppBaseID, 'Lead');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tr>
+            <th>Unique ids {`(there are ${data.length})`}</th>
+          </tr>
+          {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+            </tr>
+          ))}
+        </table>
       </header>
     </div>
   );
