37f52fd8c99df56235b9cfdf259518c82a399ebd
greg-hodgson <57120295+greg-hodgson@users.noreply.github.com>
2021-10-28 11:10:05 +1100

    change table

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <table>
+          <thead>
           <tr>
             <th>Unique ids {`(there are ${data.length})`}</th>
           </tr>
+          </thead>
+          <tbody>
           {data.map(item => (
             <tr key={item.id}>
               <td>{item.id}</td>
             </tr>
           ))}
+          </tbody>
         </table>
       </header>
     </div>
